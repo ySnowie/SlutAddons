f7f8b1d4a7dafb58d6820138e4680162311bf849
GitHub Action <action@github.com>
2024-06-01 23:37:16 +0000

    Initialize template repository proper
    Set package name to com.github.ysnowie.slutaddons
    Set project name to SlutAddons
    Set modid to slutaddons

--- src/main/java/com/github/ysnowie/slutaddons/ExampleMod.java
+++ src/main/java/com/github/ysnowie/slutaddons/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.ysnowie.slutaddons;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "slutaddons", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/ysnowie/slutaddons/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/ysnowie/slutaddons/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.ysnowie.slutaddons.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
